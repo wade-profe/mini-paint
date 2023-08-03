8ca0313eb2a164ede516613a7d3565828fe3eb51
wade-profe <wade.profe@gmail.com>
2023-08-03 07:04:40 +0200

    Initial commit

---
+++ app/src/main/java/com/example/android/minipaint/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.android.minipaint
+
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View.SYSTEM_UI_FLAG_FULLSCREEN
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val myCanvasView = MyCanvasView(this)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.setDecorFitsSystemWindows(false)
+        } else {
+            myCanvasView.systemUiVisibility = SYSTEM_UI_FLAG_FULLSCREEN
+        }
+
+        myCanvasView.contentDescription = getString(R.string.canvasContentDescription)
+
+        setContentView(myCanvasView)
+
+    }
+}
